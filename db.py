8c871bc646c0676ea473d5210c3789841c515f1e
Mostafa <mostafarasooli54@gmail.com>
2024-11-04 00:12:49 +0330

    v1.0.1
    - db variables removed from code

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, Session, create_engine
 
-DATABASE_URL = "postgresql://postgres:i3center@127.0.0.1/fastapi"
+load_dotenv()
+
+DATABASE_URL = (f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_password')}@{os.getenv('DB_HOST')}/"
+                f"{os.getenv('DB_NAME')}")
 
 engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
 
 
 def create_db_and_tables():
